320fa42bcc63c0a4816b5ac2c4da779ae6bb0e2f
skulkov <sergey.kulkov@gmail.com>
2019-02-27 11:58:54 +0300

    facade

---
+++ src/WebSK/KeyValue/KeyValueFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WebSK\KeyValue;
+
+use Websk\Slim\Facade;
+
+/**
+ * Class KeyValueFacade
+ * @see KeyValueService
+ * @method static getOptionalValueForKey(string $key, string $default_value = '')
+ * @package WebSK\KeyValue
+ */
+class KeyValueFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return KeyValue::ENTITY_SERVICE_CONTAINER_ID;
+    }
+}
